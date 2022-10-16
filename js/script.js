e3fc25a723d87860a8197e44cb678ba0b4f6ae15
Juan Romero <jprv77@gmail.com>
2022-10-16 16:54:27 -0300

    Ajustes para entrega
    Se cambiaron los archivos acorde a lo avanzado durante la clase.

--- js/script.js
+++ js/script.js
@@ -3,33 +3,18 @@ var opciones = document.getElementById('contenedor-opciones');
 var titulo = document.getElementById('titulo');
 var carrusel = document.getElementById('carousel');
 var botonnavbar= document.getElementById('boton-navbar');
-
-
+ 
+console.log(boton)
 boton.addEventListener('click', function(){
    opciones.classList.toggle('d-none');
    opciones.setAttribute('visible-data','visible');
-   titulo.classList.toggle('d-none');
-    carrusel.classList.toggle('d-none');
 })
-
+ 
 botonnavbar.addEventListener('click',function(){
     if((botonnavbar.classList.contains('collapsed')) && (opciones.hasAttribute('visible-data'))){
         console.log("hola")
-        opciones.classList.toggle('d-none');
+        opciones.classList.add('d-none');
         opciones.setAttribute('visible-data','visible');
-        titulo.classList.toggle('d-none');
-         carrusel.classList.toggle('d-none');
+ 
     }
 })
-
-/* function mostrar(){
-    if((botonnavbar.classList.contains('collapsed')) && (opciones.hasAttribute('visible-data'))){
-        console.log("hola")
-        opciones.classList.toggle('d-none');
-        opciones.setAttribute('visible-data','visible');
-        titulo.classList.toggle('d-none');
-         carrusel.classList.toggle('d-none');
-    }
-}
- */
-
